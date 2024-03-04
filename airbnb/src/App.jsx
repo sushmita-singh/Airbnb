26beffc59023ea9cb10a4a515319b6e68fe781e9
sushmita-singh <sushmitas326@gmail.com>
2024-03-04 12:47:32 +0530

    Static Layout Complete

--- airbnb/src/App.jsx
+++ airbnb/src/App.jsx
@@ -1,8 +1,16 @@
 import React from "react"
 import Nav from "../components/Nav.jsx"
+import Images from "../components/Images.jsx"
+import Main  from "../components/Main.jsx"
+import Slider from "../components/Slider.jsx"
 
 export default function App() {
     return (
-        <Nav />
+        <>
+            <Nav />
+            <Images />
+            <Main />
+            <Slider />
+        </>
     )
 }
