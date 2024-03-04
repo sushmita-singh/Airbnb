c267208a708e9420a57112ec0a4e1b18f37055a6
sushmita-singh <sushmitas326@gmail.com>
2024-03-04 15:38:00 +0530

    Card component created and replaced hard coded data

---
+++ airbnb/components/Card.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Card(props) {
+
+    let badgeText;
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.item.location === "Online") {
+        badgeText = "ONLINE";
+    }
+
+    return (
+        <div className="slider-element">
+            <div className="slider-image-section">                    
+                {props.item.openSpots === 0 && <button className="btn">{badgeText}</button>}
+                {props.item.location === "Online" && <button className="btn">{badgeText}</button>}
+                <img src={`../images/${props.item.coverImg}`} alt="slider-image" className="slider-image"/>
+            </div>
+            <div className="slider-desc"> 
+                <div className="ratings-section">
+                    <img src="../images/star.png" alt="star" className="star" />
+                    <p className="ratings">{props.item.stats.rating}<span className="light"> ({props.item.stats.reviewCount})•{props.item.location}</span></p>
+                </div>
+                <h3 className="slider-heading">{props.item.title}</h3>
+                <p className="rate"><span className="bold">From ${props.item.price}</span> /person</p>
+            </div>
+        </div>
+    )
+}
--- airbnb/components/Slider.jsx
+++ airbnb/components/Slider.jsx
@@ -1,51 +1,14 @@
 import React from "react";
+import Card from "./Card";
+import data from "../data";
 
 export default function Slider() {
+
     return (
         <div className="slider">
-            <div className="slider-element">
-                <div className="slider-image-section">                    
-                    <button className="btn">SOLD OUT</button>
-                    <img src="../images/11.jpg" alt="slider-image" className="slider-image"/>
-                </div>
-                <div className="slider-desc">
-                    <div className="ratings-section">
-                        <img src="../images/star.png" alt="star" className="star" />
-                        <p className="ratings">5.0<span className="light"> (6)•USA</span></p>
-                    </div>
-                    <h3 className="slider-heading">Life lessons with Katie Zaferes</h3>
-                    <p className="rate"><span className="bold">From $136</span> /person</p>
-                </div>
-            </div>
-
-            <div className="slider-element">
-                <div className="slider-image-section">
-                    <button className="btn">ONLINE</button>
-                    <img src="../images/12.jpg" alt="slider-image" className="slider-image"/>
-                </div>
-                <div className="slider-desc">
-                    <div className="ratings-section">
-                        <img src="../images/star.png" alt="star" className="star" />
-                        <p className="ratings">5.0<span className="light"> (30)•USA</span></p>
-                    </div>
-                    <h3 className="slider-heading">Learn wedding photography</h3>
-                    <p className="rate"><span className="bold">From $125</span> /person</p>
-                </div>
-            </div>
-
-            <div className="slider-element">
-                <div className="slider-image">
-                    <img src="../images/13.jpg" alt="slider-image" className="slider-image"/>
-                </div>
-                <div className="slider-desc">
-                    <div className="ratings-section">
-                        <img src="../images/star.png" alt="star" className="star" />
-                        <p className="ratings">4.8<span className="light"> (2)•USA</span></p>
-                    </div>
-                    <h3 className="slider-heading">Group Mountain Biking</h3>
-                    <p className="rate"><span className="bold">From $50</span> /person</p>
-                </div>
-            </div>
-    </div>
+            <Card item = {data[0]} />
+            <Card item = {data[1]}  />
+            <Card item = {data[2]}  />
+        </div>
     )
 }
